a3ca7c6a3414305ed6716ac3e6386dca7aa4c9d7
stefonsimmons <s.j.simmons@spartans.nsu.edu>
2021-09-04 01:01:54 -0400

    assets controller cleaned up

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -1,5 +1,4 @@
 class AssetsController < ApplicationController
-
   def index
     user = User.find(params[:user_id])
     @user_assets = user.assets
@@ -12,9 +11,6 @@ class AssetsController < ApplicationController
 
     render json: form_assets
   end
-
-
-
 
   private
 
@@ -30,30 +26,36 @@ class AssetsController < ApplicationController
 
   # forming assets with cmc data
   def form_assets
+    # adding most cmc info
     partial_assets = @user_assets.map do |asset|
       asset_price = @user_cmc_assets[asset["symbol"]]["quote"]["USD"]["price"]
       allocation_currency_usd_price = asset["allocation_currency"] != "USD" ? @cmc_allocation_currency_prices[asset["allocation_currency"]]["quote"]["USD"]["price"] : 1
       asset_priced_in_allocation_currency = asset_price / allocation_currency_usd_price
       value = asset.quantity * asset_priced_in_allocation_currency
       value_change = value - asset.allocation
+      percent_change = (value_change / asset.allocation) * 100
+      percent_of_port = (asset.allocation / @user_assets.sum_of_allocations(asset.allocation_currency)) * 100
+      cost_basis = asset.allocation / asset.quantity
+
       cmc_info = {
         :allocation_currency_usd_price => allocation_currency_usd_price,
-        :price => asset_price / allocation_currency_usd_price,
+        :price => asset_priced_in_allocation_currency,
         :value => value,
         :value_change => value_change,
-        :percent_change => (value_change / asset.allocation) * 100,
-        :percent_of_port => (asset.allocation / @user_assets.sum_of_allocations(asset.allocation_currency)) * 100,
-        :cost_basis => asset.allocation / asset.quantity,
+        :percent_change => percent_change,
+        :percent_of_port => percent_of_port,
+        :cost_basis => cost_basis,
       }
       asset.attributes.merge(cmc_info)
     end
 
-    sum_of_values = partial_assets.sum do |asset|
-      asset[:value]
-    end
+    # summing assets values
+    sum_of_values = partial_assets.sum { |asset| asset[:value] }
 
+    #forming complete assets
     formed_assets = partial_assets.map do |asset|
-      asset.merge({ :percent_of_curr_port => (asset["allocation"] / sum_of_values) * 100 })
+      percent_of_curr_port = (asset["allocation"] / sum_of_values) * 100
+      asset.merge({ :percent_of_curr_port => percent_of_curr_port })
     end
 
     return formed_assets
