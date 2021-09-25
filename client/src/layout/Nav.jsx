d1e54a488d09b462d1037c04c5be33d309381773
stefonsimmons <s.j.simmons@spartans.nsu.edu>
2021-09-25 19:44:18 -0400

    added login nav

--- client/src/layout/Nav.jsx
+++ client/src/layout/Nav.jsx
@@ -3,8 +3,16 @@ import { Link } from 'react-router-dom'
 export default function Nav() {
   return (
     <nav>
-      <Link to="sign-up">Sign-up</Link>
-      <Link to="sign-in">Sign-in</Link>
+      {/* <div className="links-lout">
+        <Link to="/sign-up">Sign-up</Link>
+        <Link to="/sign-in">Sign-in</Link>
+      </div> */}
+      <div className="links-lin">
+        <Link to="/ports">Ports</Link>
+        <Link to="/add-assets">Sign-in</Link>
+        <p>Stefon</p>
+        <p>Logout</p>
+      </div>
     </nav>
   )
 }
