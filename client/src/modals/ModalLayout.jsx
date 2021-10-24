765fa27fdf1ff7bc58504a8ef565ed35789c8743
stefonsimmons <s.j.simmons@spartans.nsu.edu>
2021-10-24 17:52:41 -0400

    modal close on bg

--- client/src/modals/ModalLayout.jsx
+++ client/src/modals/ModalLayout.jsx
@@ -2,7 +2,13 @@ import close from '../assets/imgs/close.svg'
 
 export default function ModalLayout(props) {
   return (
-    <div className="modal-bg">
+    <div
+      className="modal-bg"
+      onClick={() => props.updateModal(prevModal => ({
+        ...prevModal,
+        [props.modal]: false
+      }))}
+    >
       <section className='modal'>
         <header>
           <h1>{`${props.modal === 'edit' ? `${props.modal} asset`: props.modal}`}</h1>
