e8acecfd61ce0b29a0d31d6bfc1e072bf476756c
stefonsimmons <s.j.simmons@spartans.nsu.edu>
2021-10-24 15:13:23 -0400

    styled port edit component

--- client/src/modals/Ports.jsx
+++ client/src/modals/Ports.jsx
@@ -1,26 +1,54 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import ModalLayout from './ModalLayout'
 
 export default function Ports(props) {
+  const [portID, initPortEdit] = useState(null)
+  const [portData, updatePortData] = useState({
+    name: '',
+    alias: '',
+    user_id: ''
+  })
+
+  const handleChange = (e) => {
+    const { value, name } = e.target
+    console.log(name, value)
+  }
 
   return (
     <ModalLayout modal='port' updateModal={props.updateModal}>
       <div className="portfolios">
         {props.portfolios.map(portfolio => (
-          <div key={portfolio.id} className="portfolio">
-            <Link
-              to={`/portfolios/${portfolio.id}`}
-              onClick={() => {
-                props.updateModal(prevModal => ({ ...prevModal, port: false }))
-              }}
-            >
-              {portfolio.alias}
-            </Link>
-            <div className="mutables">
-              <button className="edit-btn">Edit</button>
-              <button className="delete-btn">Delete</button>
-            </div>
-          </div>
+          <>
+            {portID != portfolio.id ?
+              <div key={portfolio.id} className="portfolio">
+                <Link
+                  to={`/portfolios/${portfolio.id}`}
+                  onClick={() => {
+                    props.updateModal(prevModal => ({ ...prevModal, port: false }))
+                  }}
+                >
+                  {portfolio.alias}
+                </Link>
+
+                <div className="mutables">
+                  <button onClick={() => initPortEdit(portfolio.id)} className="edit-btn">Edit</button>
+                  <button className="delete-btn">Delete</button>
+                </div>
+              </div>
+              :
+              <form className="portfolio">
+                <input
+                  type="text"
+                  name={portData.alias} 
+                  value={portfolio.alias}
+                  onChange={(e) => handleChange(e)}
+                  className='port-alias'
+                />
+                <input type="submit" value="Save" className="save-btn" />
+              </form>
+            }
+          </>
         ))}
       </div>
     </ModalLayout>
