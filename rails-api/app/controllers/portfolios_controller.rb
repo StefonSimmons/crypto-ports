dc0b2112050bcf15ef566ba4dc107ad271609c24
stefonsimmons <s.j.simmons@spartans.nsu.edu>
2022-02-17 19:46:39 -0500

    authorize access to GET `/users/<u_id>/portfolios`

--- rails-api/app/controllers/portfolios_controller.rb
+++ rails-api/app/controllers/portfolios_controller.rb
@@ -1,5 +1,6 @@
 class PortfoliosController < ApplicationController
   before_action :set_portfolio, only: [:update, :destroy]
+  before_action :authorize_request, only: [:index]
 
   # GET /users/:user_id/portfolios
   def index
