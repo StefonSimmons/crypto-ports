747e393905f4d4bad89d757ff74d43ae472ee2a9
stefonsimmons <s.j.simmons@spartans.nsu.edu>
2022-02-18 19:07:12 -0500

    reorder fields

--- client/src/screens/Port.jsx
+++ client/src/screens/Port.jsx
@@ -36,13 +36,13 @@ export default function Port({ portfolios, updateModal, setAsset, setAssets, ass
           <h2>Asset</h2>
           <h2>Allocation</h2>
           <h2>Quantity</h2>
+          <h2>Cost Basis</h2>
           <h2>Price</h2>
           <h2>Value</h2>
           <h2>Change</h2>
           <h2>% Change</h2>
           <h2>% of Port</h2>
           <h2>% of Curr Port</h2>
-          <h2>Cost Basis</h2>
         </div>
         <hr />
         <div className="port-assets">
@@ -69,13 +69,13 @@ export default function Port({ portfolios, updateModal, setAsset, setAssets, ass
               <p>{asset.symbol}-{asset.allocation_currency}</p>
               <p>{asset.allocation.round()}</p>
               <p>{asset.quantity}</p>
+              <p>{asset.cost_basis.round()}</p>
               <p>{asset.price.round()}</p>
               <p>{asset.value.round()}</p>
               <p>{asset.value_change.round()}</p>
               <p>{`${asset.percent_change.round(false, true)}%`}</p>
               <p>{`${asset.percent_of_port.round(false, true)}%`}</p>
               <p>{`${asset.percent_of_curr_port.round(false, true)}%`}</p>
-              <p>{asset.cost_basis.round()}</p>
             </div>
             )
           })}
