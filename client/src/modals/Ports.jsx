b004630ddfc1512fa29d4effc24fc0adc654489d
stefonsimmons <s.j.simmons@spartans.nsu.edu>
2022-02-26 08:59:29 -0500

    restyle btns with icons

--- client/src/modals/Ports.jsx
+++ client/src/modals/Ports.jsx
@@ -26,62 +26,69 @@ export default function Ports(props) {
   return (
     <ModalLayout modal='ports' updateModal={props.updateModal}>
       <div className="portfolios">
-        { props.portfolios.length ?
-            props.portfolios.map(portfolio => (
-              <React.Fragment key={portfolio.id}>
-                {editPortID !== portfolio.id ?
-                  // All Ports
-                  <div className="portfolio">
-                    <Link
-                      to={`/portfolios/${portfolio.id}`}
-                      onClick={() => {
-                        props.updateModal(prevModal => ({ ...prevModal, ports: false }))
-                      }}
-                    >
-                      {portfolio.alias}
-                    </Link>
+        {props.portfolios.length ?
+          props.portfolios.map(portfolio => (
+            <React.Fragment key={portfolio.id}>
+              {editPortID !== portfolio.id ?
+                // All Ports
+                <div className="portfolio">
+                  <Link
+                    to={`/portfolios/${portfolio.id}`}
+                    onClick={() => {
+                      props.updateModal(prevModal => ({ ...prevModal, ports: false }))
+                    }}
+                  >
+                    {portfolio.alias}
+                  </Link>
 
-                    <div className="mutables">
-                      <button onClick={() => initPortEdit(portfolio.id)} className="edit-btn">Rename</button>
-                      <button
-                        onClick={() => {
-                          props.updateMsgModal({
-                            alias: portfolio.alias,
-                            id: portfolio.id
-                          })
-                        }}
-                        className="delete-btn">
-                        <span className="material-icons">
+                  <div className="mutables">
+                    <button onClick={() => initPortEdit(portfolio.id)} className="edit-btn">
+                      <span class="material-icons">
+                        drive_file_rename_outline
+                      </span></button>
+                    <button
+                      onClick={() => {
+                        props.updateMsgModal({
+                          alias: portfolio.alias,
+                          id: portfolio.id
+                        })
+                      }}
+                      className="delete-btn">
+                      <span className="material-icons">
                         delete_outline
-                        </span>
-                      </button>
-                    </div>
+                      </span>
+                    </button>
                   </div>
-                  :
-                  // Edit Form
-                  <form
-                    className="portfolio"
-                    onSubmit={(e) => {
-                      props.handleEditPort(e, editPortID, portData)
-                      initPortEdit(false)
-                    }}>
-                    <input
-                      type="text"
-                      name={portfolio.name}
-                      value={portData.alias}
-                      placeholder={portfolio.alias}
-                      onChange={(e) => handleChange(e)}
-                      className='port-alias'
-                    />
-                    <input type="submit" value="Save" className="save-btn" />
-                  </form>
-                }
-              </React.Fragment>
-            ))
+                </div>
+                :
+                // Edit Form
+                <form
+                  className="portfolio"
+                  onSubmit={(e) => {
+                    props.handleEditPort(e, editPortID, portData)
+                    initPortEdit(false)
+                  }}>
+                  <input
+                    type="text"
+                    name={portfolio.name}
+                    value={portData.alias}
+                    placeholder={portfolio.alias}
+                    onChange={(e) => handleChange(e)}
+                    className='port-alias'
+                  />
+                  <button type="submit" className="save-btn">
+                    <span class="material-icons">
+                      check
+                    </span>
+                  </button>
+                </form>
+              }
+            </React.Fragment>
+          ))
           :
-          <h2>{`You dont have any portfolios yet. Create portfolios when you `} 
+          <h2>{`You dont have any portfolios yet. Create portfolios when you `}
             <button onClick={() => {
-            props.updateModal(prevModal => ({ ...prevModal, asset: true, ports: false }))
+              props.updateModal(prevModal => ({ ...prevModal, asset: true, ports: false }))
             }}>add an asset</button>
           </h2>
         }
