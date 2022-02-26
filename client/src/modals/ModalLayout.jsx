ff95225f05ed81232d4a45f743d1964939d69daa
stefonsimmons <s.j.simmons@spartans.nsu.edu>
2022-02-25 23:38:34 -0500

    modal responsive

--- client/src/modals/ModalLayout.jsx
+++ client/src/modals/ModalLayout.jsx
@@ -1,22 +1,17 @@
-import close from '../assets/imgs/close.svg'
-
 export default function ModalLayout(props) {
   return (
     <div className="modal-bg">
       <section className='modal'>
         <section className="modal-header">
-          <h1>{`${props.modal === 'edit' ? `${props.modal} asset`: props.modal}`}</h1>
-          <img
-            src={close}
-            alt="close"
-            className="close"
-            onClick={() => {
+          <h1>{`${props.modal === 'edit' ? `${props.modal} asset` : props.modal}`}</h1>
+          <span class="material-icons close" onClick={() => {
               props.updateModal(prevModal => ({
                 ...prevModal,
                 [props.modal.replaceAll(" ", "")]: false
               }))
-            }}
-          />
+            }}>
+            close
+          </span>
         </section>
         {props.children}
       </section>
