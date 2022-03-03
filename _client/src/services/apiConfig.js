fd4216497c1e8c0cb4af91d4c197b651897b4395
stefonsimmons <s.j.simmons@spartans.nsu.edu>
2022-03-02 20:22:30 -0500

    fix typo process.env

--- _client/src/services/apiConfig.js
+++ _client/src/services/apiConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseURL = process.NODE_ENV === 'production' ? 'https://simmports-api.herokuapp.com' : 'http://localhost:3000'
+const baseURL = process.env.NODE_ENV === 'production' ? 'https://simmports-api.herokuapp.com' : 'http://localhost:3000'
 
 
 const api = axios.create({
